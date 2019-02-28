5f62128b58f2a11a4c5001f58bfd8a43f3839b14
Arhimond1998 <eduard.dokhuzhev@mail.ru>
2019-02-28 12:02:22 +0300

    Still working

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     #url('post/<int:pk>/', views.post_detail, name='post_detail'),
 	url(r'^$', views.post_list, name='post_list'),
     url(r'^(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+
 
 ]
